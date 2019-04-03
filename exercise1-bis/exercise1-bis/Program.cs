3162d666cbdd952ba3ea564758efd51aa7b9ea40
Cristian <42909882+NoToda@users.noreply.github.com>
2019-04-03 15:57:25 +0200

    Add files via upload

---
+++ exercise1-bis/exercise1-bis/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numar;
+
+            while (true)
+            {
+                Console.Write("Scrieti un numar: ");
+                string numarInput = Console.ReadLine();
+
+                bool success = int.TryParse(numarInput, out numar);
+
+                if (success)
+                {
+                    Console.WriteLine("Rezultatul este: ");
+
+                    for (int i = 0; i < 2; i++)
+                    {
+                        for (int u = 0; u < 4; u++)
+                        {
+                            Console.Write(numarInput + " ");
+                        }
+
+                        Console.WriteLine("\n");
+
+                        for (int u = 0; u < 4; u++)
+                        {
+                            Console.Write(numarInput);
+                        }
+
+                        Console.WriteLine("\n");
+
+                    }
+
+                    break;
+
+                }
+                else
+                {
+                    Console.WriteLine("This is not an integer number! Try again.");
+
+                    continue;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ exercise1/exercise1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Scrieti un numar: ");
+            string numarInput = Console.ReadLine();
+
+            Console.WriteLine("Rezultatul este: ");
+
+            Console.WriteLine($"{numarInput} {numarInput} {numarInput} {numarInput}");
+            Console.WriteLine($"{numarInput}{numarInput}{numarInput}{numarInput}");
+
+            Console.WriteLine($"{numarInput} {numarInput} {numarInput} {numarInput}");
+            Console.WriteLine($"{numarInput}{numarInput}{numarInput}{numarInput}");
+
+            Console.ReadLine();
+        }
+    }
+}
